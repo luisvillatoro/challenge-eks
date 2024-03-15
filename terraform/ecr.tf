14fa151c798d452d7aa10d94e739471ae09c69ea
luisvillatoro <lafvillatoro@gmail.com>
2024-03-14 22:58:39 -0600

    add repository secret

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "cactustech-ecr" {
-  name                 = "cactustech-ecr"
+  name                 = "cactustech-challenge"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
