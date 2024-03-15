a32010a5200d946b79ee8eed3be8d015c5ef9f7b
luisvillatoro <lafvillatoro@gmail.com>
2024-03-15 16:16:53 -0600

    Add cloudwatch add with terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,3 +23,28 @@ resource "aws_eks_addon" "ebs-csi" {
     "terraform" = "true"
   }
 }
+
+module "cw_metrics_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "~> 4.7.0"
+
+  create_role  = true
+  role_name    = "cw-observability-role"
+  provider_url = module.eks.oidc_provider
+
+  role_policy_arns = [
+    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+    "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess",
+  ]
+  number_of_role_policy_arns = 2
+
+  oidc_fully_qualified_subjects = [
+    "system:serviceaccount:amazon-cloudwatch:cloudwatch-agent",
+  ]
+}
+
+resource "aws_eks_addon" "cw_metrics" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "amazon-cloudwatch-observability"
+  service_account_role_arn = module.cw_metrics_role.iam_role_arn
+}
